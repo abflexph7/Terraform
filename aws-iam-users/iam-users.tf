27049f6bd2975433b6aae7040a44a4c8c5b06636
abflexph7 <abflex.ph7@gmail.com>
2025-11-23 14:44:12 +0000

    edited the loops

--- aws-iam-users/iam-users.tf
+++ aws-iam-users/iam-users.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_user" "users" {
-    for_each = toset(var.project-users) #variable is a list of IAM users.
-    name = each.value
+    for_each = toset(var.project-users) #variable is a list of IAM users. Will get rid of duplicates.
+    name = each.value #loop through the individual names in the list and create a user for them.
 
 }
--- for_each loop/main.tf
+++ for_each loop/main.tf
@@ -1,6 +1,6 @@
 #this for_loop is useful for creating multiple users as it does so by looping through a list in the variables.tf then creating each user
 resource "local_sensitive_file" "name" {
-    for_each = toset(var.users) #for_each loops over sets so the toset is turning var.users in to a set as it is a list at the moment
-    filename = each.value       #each value in the list is referenced as each.value
+    filename = var.users[count.index] #creates a count with index for var.users
+    count = length(var.users)       #loops through var.users
     content = var.content       #takes variable content in variables.tf file as content value. This is a password 
 }
